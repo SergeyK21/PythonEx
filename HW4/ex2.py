17d85e3f2b0fae1d63d1229ca4c60d398894e2a1
Sergey <isip88g@gmail.com>
2021-10-05 23:03:27 +0300

    Исправление ex2

--- HW4/ex2.py
+++ HW4/ex2.py
@@ -5,4 +5,4 @@
 Результат: [12, 44, 4, 10, 78, 123].
 """
 list_1 = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-print([list_1[i] for i in range(1, len(list_1)) if list_1[i] > list_1[i - 1]])
+print(*(list_1[i] for i in range(1, len(list_1)) if list_1[i] > list_1[i - 1]))
