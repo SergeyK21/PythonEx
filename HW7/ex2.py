a367901d6947f67fc83898d9e4a98b50438893ef
Sergey <isip88g@gmail.com>
2021-10-18 13:36:19 +0300

    ex2

--- HW7/ex2.py
+++ HW7/ex2.py
@@ -13,3 +13,75 @@
 проверить на практике работу декоратора @property.
 
 """
+from abc import ABC, abstractmethod
+
+
+class Cloth(ABC):
+    @abstractmethod
+    def fabric_consumption(self):
+        pass
+
+
+class Coat(Cloth):
+    def __init__(self, dimension):
+        self.dimension = dimension
+
+    def fabric_consumption(self):
+        return self.dimension / 6.5 + 0.5
+
+
+class Costume(Cloth):
+    def __init__(self, height):
+        self.height = height
+
+    def fabric_consumption(self):
+        return 2 * self.height + 0.3
+
+
+class Clothes:
+    __fabric_consumption = 0
+
+    def __init__(self, dimension=None, height=None):
+        self.height = height
+        self.dimension = dimension
+        self.list_coat = []
+        if self.dimension != None:
+            self.list_coat.append(Coat(self.dimension))
+            Clothes.__fabric_consumption += self.list_coat[0].fabric_consumption()
+        self.list_costume = []
+        if self.height != None:
+            self.list_costume.append(Costume(self.height))
+            Clothes.__fabric_consumption += self.list_costume[0].fabric_consumption()
+
+    def add_coat(self, dimension):
+        self.list_coat.append(Coat(dimension))
+        Clothes.__fabric_consumption += self.list_coat[len(self.list_coat) - 1].fabric_consumption()
+
+    def add_costume(self, height):
+        self.list_costume.append(Costume(height))
+        Clothes.__fabric_consumption += self.list_costume[len(self.list_costume) - 1].fabric_consumption()
+
+    @property
+    def general_calculation(self):
+        return Clothes.__fabric_consumption
+
+    @property
+    def quantity_coat(self):
+        return len(self.list_coat)
+
+    @property
+    def quantity_costume(self):
+        return len(self.list_costume)
+
+
+clothets = Clothes()
+clothets.add_coat(200)
+clothets.add_costume(300)
+clothets.add_coat(100)
+clothets.add_coat(120)
+clothets.add_coat(340)
+clothets.add_costume(234)
+clothets.add_costume(123)
+print(f'пальто {clothets.quantity_coat} шт.')
+print(f'костюмы {clothets.quantity_costume} шт.')
+print(clothets.general_calculation)
